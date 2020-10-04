975b2c0694e56d1d0240421a913b3ee0e6dce7cd
Nayan <nayan.pradhan@hotmail.com>
2020-10-04 11:31:50 +0200

    Added service client files

---
+++ ROS_Basics_In_5_Days/my_custom_srv_msg_pkg/src/call_bb8_move_custom_service_server.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+import rospy
+from my_custom_srv_msg_pkg.srv import MyCustomServiceMessage, MyCustomServiceMessageRequest
+
+rospy.init_node('bb8_move_custom_service_client_server_node')
+rospy.wait_for_service('/move_bb8_in_circle_custom')
+move_bb8_custom_service_client_server = rospy.ServiceProxy('/move_bb8_in_circle_custom', MyCustomServiceMessage)
+
+result = move_bb8_custom_service_client_server(MyCustomServiceMessageRequest)
+print result
