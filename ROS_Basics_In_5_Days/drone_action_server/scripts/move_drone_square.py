33bb8a079b766331ca5eebabe875227b02c37f27
Nayan <nayan.pradhan@hotmail.com>
2020-10-15 00:46:58 +0200

    Adding drone action server

---
+++ ROS_Basics_In_5_Days/drone_action_server/scripts/move_drone_square.py
@@ -0,0 +1,90 @@
+#! /usr/bin/env python
+
+import actionlib
+import rospy
+import actionlib
+
+from actionlib.msg import TestFeedback, TestResult, TestAction
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Empty
+
+class MoveDroneSquare(object):
+
+    # create message obj that are used to publish feedback and result
+    _feedback = TestFeedback()
+    _result = TestResult()
+
+    def __init__(self):
+        self._as = actionlib.SimpleActionServer("move_drone_square_action_server", self.goal_callback, TestAction, False)
+        self._as.start()
+        self.ctrl_c = False
+        self.rate = rospy.Rate(10)
+
+    def publish_once_in_cmd_vel(self, cmd):
+        while not self.ctrl_c:
+            connections = self._pub_cmd_vel.get_num_connections()
+            if connections > 0:
+                self._pub_cmd_vel.publish(cmd)
+                rospy.loginfo("Publish in cmd_vel")
+                break
+            else:
+                self.rate.sleep()
+    
+    def stop_drone(self):
+        rospy.loginfo("Stopping drone!")
+        self.move_drone.linear.x = 0
+        self.move_drone.angular.z = 0
+        self.publish_once_in_cmd_vel(self.move_drone)
+
+    def turn_drone(self):
+        rospy.loginfo("Turning!")
+        self.move_drone.linear.x = 0
+        self.move_drone.angualr.z = 1
+        self.publish_once_in_cmd_vel(move_drone)
+
+    def forward_drone(self):
+        rospy.loginfo("Moving forward!")
+        self.move_dron.linear.x = 1
+        self.move_dron.angular.z = 0
+        self.publish_once_in_cmd_vel(move_drone)
+    
+    def goal_callback(self, goal):
+        r = rospy.Rate(1)
+        success = True 
+
+        self._pub_cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
+        self.move_dron = Twist()
+        self._pub_takeoff = rospy.Publisher('/drone/takeoff', Empty, queue_size = 1)
+        self._takeoff_msg = Empty()
+        self._pub_land = rospy.Publisher('/drone/land', Empty. queue_size = 1)
+        self._land_msg = Empty()
+
+        # make the drone takeofff
+        for i in range(1,4):
+            self._pub_takeoff.publish(self._takeoff_msg)
+            rospy.loginfo("Taking offf")
+            time.sleep(1)
+        
+        sideSeconds = goal.goal
+        turnSeconds = 2
+
+        for i in xrange(0,4):
+            # check for preempt (cancelation) request
+            if self._as.is_preempt_requested():
+                rospy.login_info("Goal has been cancelled!")
+                self._as.set_preempted()
+                success = False
+                break
+
+            self.forward_drone()
+            time.sleep(sideSeconds)
+            self.turn_drone()
+            time.sleep(sideSeconds)
+
+            self._feedback_feedback = i
+
+
+if __name__ = "__maim__":
+    rospy.init_node('move_drone_square_node')
+    MoveDroneSquare()
+    rospy.spin()
