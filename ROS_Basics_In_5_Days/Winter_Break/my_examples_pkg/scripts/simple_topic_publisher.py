40a80d25d864008a994b0e0bfd7c22e7bb27719f
Nayan <nayan.pradhan@hotmail.com>
2020-12-22 08:51:22 +0100

    Finishing Basic Publisher

--- ROS_Basics_In_5_Days/Winter_Break/my_examples_pkg/scripts/simple_topic_publisher.py
+++ ROS_Basics_In_5_Days/Winter_Break/my_examples_pkg/scripts/simple_topic_publisher.py
@@ -1,15 +1,28 @@
 #! /usr/bin/env python
 
 import rospy
-from std_msgs.msg import Int32
+from geometry_msgs.msg import Twist
 
 rospy.init_node("topic_publisher")
-pub = rospy.Publisher('/counter', Int32, queue_size=1)
-rate = rospy.Rate(2)
-count = Int32()
-count.data = 0
+pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
 
-while not rospy.is_shutdown():
-    pub.publish(count)
-    count.data+=1
-    rate.sleep()
+move = Twist()
+
+while (not rospy.is_shutdown()):
+    move.linear.x = 0.5
+    move.angular.z = 0.3
+    pub.publish(move)
+
+# import rospy
+# from std_msgs.msg import Int32
+
+# rospy.init_node("topic_publisher")
+# pub = rospy.Publisher('/counter', Int32, queue_size=1)
+# rate = rospy.Rate(2)
+# count = Int32()
+# count.data = 0
+
+# while not rospy.is_shutdown():
+#     pub.publish(count)
+#     count.data+=1
+#     rate.sleep()
