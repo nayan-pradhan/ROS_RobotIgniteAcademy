2c1526be3596f06907f2f942f04626a4e238011d
Nayan <nayan.pradhan@hotmail.com>
2020-12-22 08:21:44 +0100

    Making seperate file for winter break

--- ROS_Basics_In_5_Days/my_package/simple.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-
-import rospy
-
-rospy.init_node('ObiWan')
-rate = rospy.Rate(0.5)
-while not rospy.is_shutdown():
-    print "Help me Obi-Wan Kenobi, you're my only hope"
-    rate.sleep()
