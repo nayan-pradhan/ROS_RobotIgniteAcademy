ed5d61fb0b7f36a0ef7a602276d2cb333068a11c
Nayan <nayan.pradhan@hotmail.com>
2020-12-24 09:31:29 +0100

    Creating a publisher and subscriber

--- ROS_Basics_In_5_Days/Winter_Break/topics_quiz/script/my_script.py
+++ ROS_Basics_In_5_Days/Winter_Break/topics_quiz/script/my_script.py
@@ -1,5 +1,11 @@
 #! /usr/bin/env python
 
 import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import LaserScan
 
 rospy.init_node("topics_quiz_node")
+pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
+sub = rospy.Subscriber("/kobuki/laser/scan", LaserScan, callback)
+
+rospy.spin()
