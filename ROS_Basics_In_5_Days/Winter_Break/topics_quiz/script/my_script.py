56d21d77584ac29037b6d5f9c816f435f7b5bee9
Nayan <nayan.pradhan@hotmail.com>
2020-12-24 09:48:59 +0100

    Finishing Topics Quiz

--- ROS_Basics_In_5_Days/Winter_Break/topics_quiz/script/my_script.py
+++ ROS_Basics_In_5_Days/Winter_Break/topics_quiz/script/my_script.py
@@ -4,8 +4,30 @@ import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
-rospy.init_node("topics_quiz_node")
-pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
-sub = rospy.Subscriber("/kobuki/laser/scan", LaserScan, callback)
+rospy.init_node("topics_quiz_node") # initialize node
+
+move = Twist() # creating twist object
+
+lin_vel = 0.0
+ang_vel = 0.4
+
+def callback(msg):
+	if msg.ranges[360] > 1: # if object to the front is more than a distance of 1
+		move.linear.x = lin_vel
+		move.angular.y = 0.0
+	if msg.ranges[360] < 1: # if object to the front is at a distance smaller than 1
+		move.linear.x = 0.0
+		move.angular.z = ang_vel
+	if msg.ranges[0] < 1: # if object to the right is at a distance smaller than 1
+		move.linear.x = 0.0
+		move.angular.z = ang_vel
+	if msg.ranges[719] < 1: # if object to the left is at a distance smaller than 1
+		move.linear.x = 0.0
+		move.angular.z = -ang_vel
+	pub.publish(move)
+
+
+pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1) # publisher
+sub = rospy.Subscriber("/kobuki/laser/scan", LaserScan, callback) # subscriber
 
 rospy.spin()
