e966a82c7afc9c0efb283d83eb1cacfd539d1a01
Nayan <nayan.pradhan@hotmail.com>
2021-01-03 10:33:43 +0100

    Adding comment in callback function

--- ROS_Basics_In_5_Days/Winter_Break/my_examples_pkg/scripts/simple_service_server.py
+++ ROS_Basics_In_5_Days/Winter_Break/my_examples_pkg/scripts/simple_service_server.py
@@ -6,6 +6,7 @@ from std_srvs.srv import Empty, EmptyResponse
 def callback(request):
     print "Callback has been called"
     return EmptyResponse()
+    #return MyServiceResponse(len(request.words.split())) 
 
 rospy.init_node("service_server")
 my_service = rospy.Service("/my_service", Empty, callback)
