81a1913684b6fd09eb8330c3c47b313aa0b8f250
Nayan <nayan.pradhan@hotmail.com>
2021-01-04 14:42:24 +0100

    Working on service quiz

--- ROS_Basics_In_5_Days/Winter_Break/services_quiz/src/bb8_move_custom_service_server.py
+++ ROS_Basics_In_5_Days/Winter_Break/services_quiz/src/bb8_move_custom_service_server.py
@@ -8,18 +8,23 @@ def callback(request):
     dist = request.side * 2 # times 2 just so that the square is not too small
     rep = request.repetition
     response = BB8CustomServiceMessageResponse()
+
     if (dist <= 0 or rep <= 0):
         response.success = False
         return response
-    i = 0
+
+    i = 0 # how many times we are repeting square
     while i < rep:
-        j = 0
+        j = 0 # four sides of square
         while j < 4:
             move_straight()
             rospy.sleep(dist)
-            turn_right()        
+            turn_right()   
+            rospy.sleep(1)     
             j += 1
         i += 1
+    response.success = True
+    return response
 
 def move_straight():
     move.linear.x = 0.5
@@ -31,7 +36,7 @@ def turn_right():
     move.angular.z = 0.5
     pub.publish(move)
 
-rospy.init_node("")
+rospy.init_node("/node_bb8_move_custom_service_server")
 my_service = rospy.Service("/move_bb8_in_square_custom", BB8CustomServiceMessage, callback)
 pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
 move = Twist()
